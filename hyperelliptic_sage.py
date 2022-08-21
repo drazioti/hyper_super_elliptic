dee5025748599210ca2c535b3a480ead0dbb23c1
drazioti <drazioti@csd.auth.gr>
2022-08-21 20:17:46 +0300

    Update hyperelliptic_sage.py

--- hyperelliptic_sage.py
+++ hyperelliptic_sage.py
@@ -61,3 +61,64 @@ def C(L,g): # L=[A,B,C] : y^2=x^3+Ax^2+Bx+C
             SOLS.append([z,sqrt(S)])
     print SOLS
         
+'''
+sage:C([0,1,2],(x + 40)*(x + 4)*(x - 4)*(x - 7))
+# we are going to solve y^2=x*(x-1)*(x-2)*(x + 40)*(x + 4)*(x - 4)*(x - 7)
+...[[16, 20160], [-7, 2772]]
+
+'''
+
+
+def C_(L,g): # L=[a1,a2,a3] : y^2=(x-a1)(x-a2)(x-a3)
+    a1,a2,a3=L[0],L[1],L[2]
+    f = (x-a1)*(x-a2)*(x-a3)
+    f=f.expand()
+    A,B,C=f.coefficient(x,2),f.coefficient(x,1),f.coefficient(x,0)
+    
+    def sf_divisors(N):
+        if is_prime(N):
+            return [1,N]
+        L = []
+        div = divisors(N)
+        for x in div:
+            if is_squarefree(x):
+                L.append(x)
+        return L
+    
+    f = x^3 + A*x^2 + B*x + C
+    var('u,v')
+    f1 = u^2 - f
+    g1 = v^2 - g
+    R0 = f1.resultant(g1,x).subs(u=0,v=0)
+    SF_d = sf_divisors(abs(int(R0)))
+    sols = []
+    for d in SF_d:
+        d = int(d)
+        E = EllipticCurve([0,int(A)*int(d),0,int(B)*int(d)^2,int(C)*int(d)^3]) # y^2 = x^3 + A*d*x^2 + B*d^2*x + d^3*C
+        L = E.integral_points()
+        L1 = []
+        for P in L: 
+            if P[1]!=0: # if y!=0
+                L1.append(P)
+        for Q in L1: # Q are integer points on E with y!=0
+            if int(Q[0])%d==0:
+                sols.append(Q[0]/d)        
+    for d1 in SF_d:
+        d = -d1
+        E = EllipticCurve([0,int(A)*int(d),0,int(B)*int(d)^2,int(C)*int(d)^3])
+        L = E.integral_points()
+        L1 = []
+        for P in L:
+            if P[1]!=0:
+                L1.append(P)
+        for Q in L1:
+            if int(Q[0])%d==0:
+                sols.append(Q[0]/d)
+                
+    H =  f * g
+    SOLS = []
+    for z in sols:
+        S = H.subs(x=z)
+        if is_square(S) and S!=0:
+            SOLS.append([z,sqrt(S)])
+    print SOLS 
